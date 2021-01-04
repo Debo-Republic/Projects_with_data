5c6e370e49a764a03c757a0893cd2c7bff6a4590
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2021-01-03 22:50:48 -0500

    Add files via upload
    updated result display

--- Recommendation system using fastai/recommend_movie.py
+++ Recommendation system using fastai/recommend_movie.py
@@ -9,3 +9,6 @@ dls = CollabDataLoaders.from_csv(path/'ratings.csv')
 # Give ratings caps to the dataset 
 learn = collab_learner(dls, y_range=(0.5,5.5))
 learn.fine_tune(10)
+
+#To see the outcomes
+learn.show_results()
