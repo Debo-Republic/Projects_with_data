974eb635fdae8ea9b966d20f566b42f21fc7a2cf
Debopriyo Bhowmick <66901287+Debo-Republic@users.noreply.github.com>
2021-01-03 22:48:47 -0500

    Add files via upload
    recommend movie using collaborative filtering library from fastai

---
+++ Recommendation system using fastai/recommend_movie.py
@@ -0,0 +1,11 @@
+# Predicting movies based on people's reviews
+
+# Import the collaborative filter design from fastai library
+from fastai.collab import *
+# Decompress the data
+path = untar_data(URLs.ML_SAMPLE)
+# Split the data nunch
+dls = CollabDataLoaders.from_csv(path/'ratings.csv')
+# Give ratings caps to the dataset 
+learn = collab_learner(dls, y_range=(0.5,5.5))
+learn.fine_tune(10)
